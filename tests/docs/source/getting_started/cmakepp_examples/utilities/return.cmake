7d623ec36bd1245b7fb3beaf3641c633ebc067b6
zachcran <zachcran@gmail.com>
2022-12-20 17:07:01 -0600

    Add user documentation

--- tests/docs/source/getting_started/cmakepp_examples/utilities/return.cmake
+++ tests/docs/source/getting_started/cmakepp_examples/utilities/return.cmake
@@ -9,6 +9,16 @@ function(fxn_with_return return_identifier)
 
 endfunction()
 
+# Define a function with a multiple return values
+function(fxn_with_multiple_returns return_id_1 return_id_2)
+
+    set("${return_id_1}" "test_value_1")
+    set("${return_id_2}" "test_value_2")
+
+    cpp_return("${return_id_1}" "${return_id_2}")
+
+endfunction()
+
 ct_add_test(NAME "return")
 function("${return}")
 
@@ -19,5 +29,15 @@ function("${return}")
     # OUTPUT: test_value
 
     ct_assert_equal(return_value "test_value")
+
+    fxn_with_multiple_returns(rv_1 rv_2)
+
+    message("${rv_1}")
+    # OUTPUT: test_value_1
+    message("${rv_2}")
+    # OUTPUT: test_value_2
+
+    ct_assert_equal(rv_1 "test_value_1")
+    ct_assert_equal(rv_2 "test_value_2")
 
 endfunction()
