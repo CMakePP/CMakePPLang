8b5c4bb1d15555e099810834dced664f01335199
zachcran <zachcran@gmail.com>
2022-12-21 10:52:51 -0600

    Added documentation and testing for cpp_check_conflicting_types

---
+++ tests/docs/source/getting_started/cmakepp_examples/utilities/check_conflicting_variable_type.cmake
@@ -0,0 +1,19 @@
+include(cmake_test/cmake_test)
+
+ct_add_test(NAME "check_conflicting_variable_type")
+function("${check_conflicting_variable_type}")
+
+    set(string_to_check "target")
+
+    # Get the type of a value and print the result
+    cpp_check_conflicting_types(is_conflict conflicting_type ${string_to_check})
+    message("Is there a conflict? ${is_conflict}")
+    message("'${string_to_check}' conflicts with type: ${conflicting_type}")
+
+    # Output: Is there a conflict? TRUE
+    #         'target' conflicts with type: target
+
+    ct_assert_equal(is_conflict TRUE)
+    ct_assert_equal(conflicting_type "target")
+
+endfunction()
