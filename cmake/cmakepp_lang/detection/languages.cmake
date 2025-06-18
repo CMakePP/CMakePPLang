446ec52926314099a59a9d22118477f9750170ce
Ryan M. Richard <rrichard@ameslab.gov>
2025-06-18 08:50:31 -0500

    suggested review changes

--- cmake/cmakepp_lang/detection/languages.cmake
+++ cmake/cmakepp_lang/detection/languages.cmake
@@ -18,26 +18,31 @@ include(cmakepp_lang/utilities/return)
 #[[[
 # Wraps the process of retrieving a list of enabled languages.
 #
-# CMake sets a global property ``ENABLED_LANGUAGES`` to be a list of coding
-# languages which have been enabled either via ``project()`` or the
-# ``enable_language()`` commands. CMake's documentation is a bit dodgy in 
+# CMake sets a global property
+# :ref:`ENABLED_LANGUAGES <https://cmake.org/cmake/help/latest/prop_gbl/ENABLED_LANGUAGES.html>` 
+# to be a list of coding
+# languages which have been enabled either via 
+# :ref:`project() <https://cmake.org/cmake/help/latest/command/project.html>` or 
+# the
+# :ref:`enable_language() <https://cmake.org/cmake/help/latest/command/enable_language.html>` 
+# commands. CMake's documentation is a bit dodgy in 
 # discussing what variables the aforementioned commands set, so we wrote this
 # function as a wrapper around the logic in the event that CMake changes how
 # they do things. At the very least, this function should be more user-
 # friendly.
 #
-# :param: rv A variable to hold the resulting list.
+# :param: return_variable A variable to hold the resulting list.
 # :type: list*
 #]]
-function(cpp_enabled_languages _el_rv)
-    get_property("${_el_rv}" GLOBAL PROPERTY ENABLED_LANGUAGES)
+function(cpp_enabled_languages _el_return_variable)
+    get_property("${_el_return_variable}" GLOBAL PROPERTY ENABLED_LANGUAGES)
 
     # CMake apparently doesn't remove NONE from the list if other languages are
     # also in the list...
-    list(LENGTH "${_el_rv}" _el_length)
+    list(LENGTH "${_el_return_variable}" _el_length)
     if("${_el_length}" GREATER 1)
-        list(REMOVE_ITEM "${_el_rv}" "NONE")
+        list(REMOVE_ITEM "${_el_return_variable}" "NONE")
     endif()
 
-    cpp_return("${_el_rv}")
+    cpp_return("${_el_return_variable}")
 endfunction()
